6a099ee1b35bd5e868b91083ec29404d21531dd0
romasmi <rsmarsu@gmail.com>
2025-11-19 01:07:12 +0700

    Implement minimal working version.

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(packed string) (string, error) {
+	var unpacked strings.Builder
+	var lastRune rune
+	for _, v := range packed {
+		if unicode.IsDigit(v) {
+			if unicode.IsDigit(lastRune) {
+				return "", fmt.Errorf("%w: number are not accepted", ErrInvalidString)
+			}
+
+			if lastRune == 0 {
+				return "", fmt.Errorf("%w: digit can not be before a substring", ErrInvalidString)
+			}
+			digit, _ := strconv.Atoi(string(v))
+			unpacked.WriteString(unpackSubstring(lastRune, digit))
+			lastRune = 0
+		} else {
+			if lastRune != 0 {
+				unpacked.WriteRune(lastRune)
+			}
+			lastRune = v
+		}
+	}
+	if lastRune != 0 {
+		unpacked.WriteRune(lastRune)
+	}
+	return unpacked.String(), nil
+}
+
+func unpackSubstring(symbol rune, count int) string {
+	var output strings.Builder
+	for i := 0; i < count; i++ {
+		output.WriteRune(symbol)
+	}
+	return output.String()
 }
