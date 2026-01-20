13071c42133b3cddba5980e5c5e83780dedf5389
romasmi <rsmarsu@gmail.com>
2026-01-21 01:20:06 +0700

    Implement workerpool.

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,9 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +13,48 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if n < 1 {
+		return fmt.Errorf("invalid workers count")
+	}
+
+	if m <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+
+	jobs := make(chan Task, len(tasks))
+	errsCounter := &atomic.Int64{}
+	go func() {
+		for _, v := range tasks {
+			jobs <- v
+			if errsCounter.Load() >= int64(m) {
+				return
+			}
+		}
+		close(jobs)
+	}()
+
+	wg := sync.WaitGroup{}
+
+	for range n {
+		wg.Go(func() {
+			worker(jobs, errsCounter, m)
+		})
+	}
+	wg.Wait()
+
+	if errsCounter.Load() >= int64(m) {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
+}
+
+func worker(jobs <-chan Task, errCounter *atomic.Int64, maxErrCount int) {
+	for job := range jobs {
+		if job() != nil {
+			errCounter.Add(1)
+		}
+		if errCounter.Load() >= int64(maxErrCount) {
+			return
+		}
+	}
 }
