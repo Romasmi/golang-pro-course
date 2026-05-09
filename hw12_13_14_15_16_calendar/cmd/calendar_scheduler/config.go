28dff509190e9316c89bec89029e94571f76d8c4
romasmi <rsmarsu@gmail.com>
2026-05-09 08:11:01 +0700

    HW14: Add scheduler.

---
+++ hw12_13_14_15_16_calendar/cmd/calendar_scheduler/config.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Logger    LoggerConf    `yaml:"logger"`
+	Storage   StorageConf   `yaml:"storage"`
+	RabbitMQ  RabbitMQConf  `yaml:"rabbitmq"`
+	Scheduler SchedulerConf `yaml:"scheduler"`
+}
+
+type LoggerConf struct {
+	Level string `yaml:"level"`
+}
+
+type StorageConf struct {
+	Type          string `yaml:"type"`
+	DSN           string `yaml:"dsn"`
+	MigrationsDir string `yaml:"migrationsDir"`
+}
+
+type RabbitMQConf struct {
+	URL   string `yaml:"url"`
+	Queue string `yaml:"queue"`
+}
+
+type SchedulerConf struct {
+	ScanInterval  time.Duration `yaml:"scan_interval"`
+	CleanInterval time.Duration `yaml:"clean_interval"`
+}
+
+func NewConfig(path string) (Config, error) {
+	config := Config{}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return config, fmt.Errorf("failed to read config file: %w", err)
+	}
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return config, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+	return config, nil
+}
